52cb18f596b213806323038d9d78aa465f67bf72
Владислав Торганенко <vladtorg.anatol@gmail.com>
2026-04-09 10:28:08 +0300

    Update zd1.cpp

--- zd1.cpp
+++ zd1.cpp
@@ -3,15 +3,17 @@
 using namespace std;
 
 int main() {
+    // Настройка корректного отображения русского языка в консоли
     setlocale(LC_ALL, "Russian");
 
-    int n;
-    int i;
-    cin >> n;
+    int number; // Объявляем переменную для хранения числа
+    cin >> number; // Считываем число с клавиатуры
 
-    for(i = 1; i <= n; i++)
-    { 
-        cout << i << " ";
-    }
+    if (number > 0) {
+        cout << "Положительное" << endl;
+    } // Ваш код:
+
+
+
     return 0;
 }
