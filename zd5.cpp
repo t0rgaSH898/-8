a90f477138ddb297b773626a9281c82c3b1bb896
Владислав Торганенко <vladtorg.anatol@gmail.com>
2026-04-09 10:30:28 +0300

    Update zd5.cpp

--- zd5.cpp
+++ zd5.cpp
@@ -1,21 +1,16 @@
 #include <iostream>
+
 using namespace std;
 
 int main() {
-    int N;
-    long long factorial = 1; 
+    int number;
+    cin >> number;
 
-    cin >> N;
-    
-    if (N < 0) {
-        return 1;
-    }
-    
-    for (int i = 1; i <= N; i++) {
-        factorial *= i;
-    }
-    
-    cout << factorial << endl;
+    if (number >= 1 && number <= 10) {
+        cout << "Число принадлежит диапазону" << endl;
+    } else {
+        cout << "Число не принадлежит диапазону" << endl;
+    } 
     
     return 0;
 }
